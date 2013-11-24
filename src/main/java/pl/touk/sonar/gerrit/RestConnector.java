90e706dcf453382a330e4db19cc394bb15a500c0
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2013-11-24 23:38:07 +0100

    Added Apache components.

---
+++ src/main/java/pl/touk/sonar/gerrit/RestConnector.java
@@ -0,0 +1,12 @@
+package pl.touk.sonar.gerrit;
+
+import java.util.List;
+
+public class RestConnector {
+
+    private static final String GET_LIST_FILES_URL_FORMAT = "/changes/%s/revisions/%s/files/";
+
+    public List<String> listFiles(String host, String port, String username) {
+
+    }
+}
