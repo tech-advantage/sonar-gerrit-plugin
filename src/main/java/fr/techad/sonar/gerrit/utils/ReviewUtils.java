faf2406caefaf9b505ce1959e8852465fbe07ad4
mildis <igardais@yahoo.fr>
2016-08-15 21:21:48 +0200

    Use org.sonar.api.batch.Severity. Simplified the ReviewUtils.thresholdToValue() and valueToThreshold() methods. Add to find a better way in GerritPlugin to convert Severity.values() from Severity[] to List<String> appart from listing the values one by one.
    Change-Id: I3af073307af5fb3d40808428a1a2ca5dacd6934d

--- src/main/java/fr/techad/sonar/gerrit/utils/ReviewUtils.java
+++ src/main/java/fr/techad/sonar/gerrit/utils/ReviewUtils.java
@@ -1,7 +1,6 @@
 package fr.techad.sonar.gerrit.utils;
 
-import org.apache.commons.lang3.StringUtils;
-import org.sonar.api.rule.Severity;
+import org.sonar.api.batch.rule.Severity;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
@@ -13,19 +12,13 @@ public final class ReviewUtils {
 
     public int thresholdToValue(String threshold) {
         int thresholdValue = ReviewFileComment.UNKNOWN_VALUE;
-
-        if (StringUtils.equals(threshold, Severity.INFO)) {
-            thresholdValue = ReviewFileComment.INFO_VALUE;
-        } else if (StringUtils.equals(threshold, Severity.MINOR)) {
-            thresholdValue = ReviewFileComment.MINOR_VALUE;
-        } else if (StringUtils.equals(threshold, Severity.MAJOR)) {
-            thresholdValue = ReviewFileComment.MAJOR_VALUE;
-        } else if (StringUtils.equals(threshold, Severity.CRITICAL)) {
-            thresholdValue = ReviewFileComment.CRITICAL_VALUE;
-        } else if (StringUtils.equals(threshold, Severity.BLOCKER)) {
-            thresholdValue = ReviewFileComment.BLOCKER_VALUE;
-        } else {
-            thresholdValue = ReviewFileComment.UNKNOWN_VALUE;
+        
+        try {
+        	thresholdValue = Severity.valueOf(threshold).ordinal();
+        }
+        catch (Exception e) {
+        	LOG.warn("[GERRIT PLUGIN] Cannot convert threshold String {} to int. Using UNKNOWN.", threshold);
+        	thresholdValue = ReviewFileComment.UNKNOWN_VALUE;
         }
 
         LOG.debug("[GERRIT PLUGIN] {} is converted to {}", threshold, thresholdValue);
@@ -35,27 +28,14 @@ public final class ReviewUtils {
 
     public String valueToThreshold(int value) {
         String threshold = UNKNOWN;
-
-        switch (value) {
-        case ReviewFileComment.INFO_VALUE:
-            threshold = Severity.INFO;
-            break;
-        case ReviewFileComment.MINOR_VALUE:
-            threshold = Severity.MINOR;
-            break;
-        case ReviewFileComment.MAJOR_VALUE:
-            threshold = Severity.MAJOR;
-            break;
-        case ReviewFileComment.CRITICAL_VALUE:
-            threshold = Severity.CRITICAL;
-            break;
-        case ReviewFileComment.BLOCKER_VALUE:
-            threshold = Severity.BLOCKER;
-            break;
-
-        default:
-            break;
+        
+        try {
+        	threshold = Severity.values()[value].toString();
         }
+        catch (Exception e){
+        	LOG.warn("[GERRIT PLUGIN] Cannot convert threshold int {} to String. Using UNKNOWN.", value);
+        }
+
         LOG.debug("[GERRIT PLUGIN] {} is converted to {}", value, threshold);
 
         return threshold;
